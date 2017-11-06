545603fee88bcf4d89a3184f1c816c110e3dc71f
ymguo <naturejolanthe@126.com>
2017-11-06 14:33:02 +0800

    bug fix

--- src/mixins/wechat.js
+++ src/mixins/wechat.js
@@ -8,13 +8,13 @@ export default {
     // 禁止微信的传播类按钮
     if (typeof WeixinJSBridge === 'undefined') {
       if (document.addEventListener) {
-        document.addEventListener('WeixinJSBridgeReady', this.onBridgeReady, false)
+        document.addEventListener('WeixinJSBridgeReady', this._onBridgeReady, false)
       } else if (document.attachEvent) {
-        document.attachEvent('WeixinJSBridgeReady', this.onBridgeReady)
-        document.attachEvent('onWeixinJSBridgeReady', this.onBridgeReady)
+        document.attachEvent('WeixinJSBridgeReady', this._onBridgeReady)
+        document.attachEvent('onWeixinJSBridgeReady', this._onBridgeReady)
       }
     } else {
-      this.onBridgeReady()
+      this._onBridgeReady()
     }
   }
 }
